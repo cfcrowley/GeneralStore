af228b99cde9e23beef1c2e98656f1a8a9e70aa8
Chris Crowley <cfcrowely42@gmail.com>
2021-09-28 09:21:21 -0400

    added Post method

---
+++ GeneralStore.MVC/GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeneralStore.MVC.Models;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Post : Product
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
