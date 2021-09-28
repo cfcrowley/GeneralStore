d10ab8eaba821845acb9bdec613ef701ecb9a908
Chris Crowley <cfcrowely42@gmail.com>
2021-09-28 11:32:09 -0400

    added delete customer

---
+++ GeneralStore.MVC/GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GeneralStore.MVC.Models;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+
+        // Get: Customer
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // Post: Customer
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // Get : Delete
+        // Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        // Post : Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Customer customer = _db.Customers.Find(id);
+            _db.Customers.Remove(customer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
