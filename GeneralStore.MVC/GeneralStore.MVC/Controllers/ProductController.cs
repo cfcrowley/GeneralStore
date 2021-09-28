9aa3a42910d1f463b25a80f06a76d89921a527d7
Chris Crowley <cfcrowely42@gmail.com>
2021-09-28 09:30:20 -0400

    added delete view and function

--- GeneralStore.MVC/GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/GeneralStore.MVC/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GeneralStore.MVC.Models;
@@ -34,6 +35,35 @@ namespace GeneralStore.MVC.Controllers
             }
 
             return View(product);
+        }
+
+        // Get : Delete
+        // Product/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        // Post : Delete
+        // Product/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Product product = _db.Products.Find(id);
+            _db.Products.Remove(product);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
